5752cfe6d83ad62ccd8f14e57af95a10d553d3e7
piyushsborse <piyushsborse25@gmail.com>
2025-03-22 21:29:12 +0530

    Changed the mongo properties

--- src/main/java/com/mongo/bill_service/consts/Consts.java
+++ src/main/java/com/mongo/bill_service/consts/Consts.java
@@ -64,7 +64,6 @@ public class Consts {
 			    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 			</body>
 			</html>
-
 				""";
 
 	public static String getSplitHTML(SplitResponse splitResponse) {
